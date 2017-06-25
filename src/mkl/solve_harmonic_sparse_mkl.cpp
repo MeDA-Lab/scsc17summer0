4c6de93f208d5d71d54bd415806f00fa70d832f6
Mu Yang <emfomy@gmail.com>
2017-06-25 21:12:07 +0800

    Updates.

--- src/mkl/solve_harmonic_sparse_mkl.cpp
+++ src/mkl/solve_harmonic_sparse_mkl.cpp
@@ -11,9 +11,14 @@
 using namespace std;
 
 void solveHarmonicSparse(
-  const int nv, const int nb,
-  const double *Lii_val, const int *Lii_row, const int *Lii_col,
-  const double *Lib_val, const int *Lib_row, const int *Lib_col,
+  const int nv,
+  const int nb,
+  const double *Lii_val,
+  const int *Lii_row,
+  const int *Lii_col,
+  const double *Lib_val,
+  const int *Lib_row,
+  const int *Lib_col,
   double *U
 ) {
   int ni=nv-nb;

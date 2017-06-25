eeee1187b9d7a9363b4d1286df621bed48f77ab5
Mu Yang <emfomy@gmail.com>
2017-06-25 15:34:59 +0800

    Import codes.

---
+++ src/main.cpp
@@ -0,0 +1,81 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file    main.cpp
+/// @brief   The main function.
+///
+/// @author  Mu Yang <<emfomy@gmail.com>>
+///
+
+#include <iostream>
+#include <harmonic.hpp>
+#include <timer.hpp>
+#include <fstream>
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @brief  Main function
+///
+int main( int argc, char** argv ) {
+
+  const char *input  = "input.obj";
+  const char *output = "output.obj";
+  Method method  = Method::KIRCHHOFF;
+
+  int nv, nf, nb, *F = nullptr, *idx_b;
+  double timer, *V = nullptr, *C = nullptr, *L, *U;
+
+  // Read arguments
+  readArgs(argc, argv, input, output, method);
+
+  // Read object
+  readObject(input, &nv, &nf, &V, &C, &F);
+
+  cout << endl;
+
+  // Verify boundary
+  idx_b = new int[nv];
+  cout << "Verifying boundary ....................." << flush;
+  tic(&timer);
+  verifyBoundary(nv, nf, F, &nb, idx_b); cout << " Done.  ";
+  toc(&timer);
+
+  // Reorder vertices
+  cout << "Reordering vertices ...................." << flush;
+  tic(&timer);
+  reorderVertex(nv, nb, nf, idx_b, V, C, F); cout << " Done.  ";
+  toc(&timer);
+
+  // Construct Laplacian
+  L = new double[nv * nv];
+  cout << "Constructing Laplacian ................." << flush;
+  tic(&timer);
+  constructLaplacian(method, nv, nf, V, F, L); cout << " Done.  ";
+  toc(&timer);
+
+  // Map boundary
+  U = new double[2 * nv];
+  cout << "Mapping Boundary ......................." << flush;
+  tic(&timer);
+  mapBoundary(nv, nb, V, U); cout << " Done.  ";
+  toc(&timer);
+
+  // Solve harmonic
+  cout << "Solving Harmonic ......................." << flush;
+  tic(&timer);
+  solveHarmonic(nv, nb, L, U); cout << " Done.  ";
+  toc(&timer);
+
+  cout << endl;
+
+  // Write object
+  writeObject(output, nv, nf, U, C, F);
+
+  // Free memory
+  delete[] V;
+  delete[] C;
+  delete[] F;
+  delete[] L;
+  delete[] U;
+  delete[] idx_b;
+
+  return 0;
+}
